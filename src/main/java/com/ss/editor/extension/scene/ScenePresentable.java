cd279328c20106b3b8136da23a3a6e67809bc8f9
Alex Brui <javasabr@gmail.com>
2017-07-28 11:44:30 +0300

    added scene presentable interface.

---
+++ src/main/java/com/ss/editor/extension/scene/ScenePresentable.java
@@ -0,0 +1,81 @@
+package com.ss.editor.extension.scene;
+
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The interface to implement presentation of some object in a scene.
+ *
+ * @author JavaSaBr
+ */
+public interface ScenePresentable {
+
+    enum PresentationType {
+        SPHERE,
+    }
+
+    /**
+     * Get the current location of this object.
+     *
+     * @return the current location.
+     */
+    @NotNull
+    default Vector3f getLocation() {
+        return Vector3f.ZERO;
+    }
+
+    /**
+     * Get the current rotation of this object.
+     *
+     * @return the current rotation.
+     */
+    @NotNull
+    default Quaternion getRotation() {
+        return Quaternion.IDENTITY;
+    }
+
+    /**
+     * Get the current scale of this object.
+     *
+     * @return the current scale.
+     */
+    @NotNull
+    default Vector3f getScale() {
+        return Vector3f.UNIT_XYZ;
+    }
+
+    /**
+     * Set the new location of this object.
+     *
+     * @param location the new location.
+     */
+    default void setLocation(@NotNull Vector3f location) {
+    }
+
+    /**
+     * Set the new scale of this object.
+     *
+     * @param scale the new scale.
+     */
+    default void setScale(@NotNull Vector3f scale) {
+    }
+
+    /**
+     * Set the new rotation of this object.
+     *
+     * @param rotation the new rotation.
+     */
+    default void setRotation(@NotNull Quaternion rotation) {
+    }
+
+    /**
+     * Get the type of presentation of this object.
+     *
+     * @return the type of presentation.
+     */
+    @NotNull
+    default PresentationType getPresentationType() {
+        return PresentationType.SPHERE;
+    }
+}
--- src/main/java/com/ss/editor/extension/scene/filter/impl/EditableLocalWaterFilter.java
+++ src/main/java/com/ss/editor/extension/scene/filter/impl/EditableLocalWaterFilter.java
@@ -2,10 +2,12 @@ package com.ss.editor.extension.scene.filter.impl;
 
 import static com.ss.editor.extension.property.EditablePropertyType.FLOAT;
 import static com.ss.editor.extension.property.EditablePropertyType.VECTOR_3F;
+import static java.lang.Math.max;
 import com.jme3.math.Vector3f;
 import com.jme3.water.WaterFilter;
 import com.ss.editor.extension.property.EditableProperty;
 import com.ss.editor.extension.property.SimpleProperty;
+import com.ss.editor.extension.scene.ScenePresentable;
 import com.ss.rlib.util.array.Array;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,10 +16,33 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author JavaSaBr
  */
-public class EditableLocalWaterFilter extends EditableWaterFilter {
+public class EditableLocalWaterFilter extends EditableWaterFilter implements ScenePresentable {
 
     public EditableLocalWaterFilter() {
         setCenter(new Vector3f());
+        setShapeType(AreaShape.Circular);
+    }
+
+    @NotNull
+    @Override
+    public Vector3f getLocation() {
+        return getCenter();
+    }
+
+    @NotNull
+    @Override
+    public Vector3f getScale() {
+        return new Vector3f(getRadius(), getRadius(), getRadius());
+    }
+
+    @Override
+    public void setLocation(@NotNull final Vector3f location) {
+        setCenter(location.clone());
+    }
+
+    @Override
+    public void setScale(@NotNull final Vector3f scale) {
+        setRadius(max(max(scale.getX(), scale.getY()), scale.getZ()));
     }
 
     @Override
